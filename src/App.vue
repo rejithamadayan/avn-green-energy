9e783eae882ca02dec69a0bad6eb02f67ed75dfc
rejith amadayan <rejithamadayan@gmail.com>
2021-09-08 13:46:11 +0530

    with first section

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <AvnHomePage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AvnHomePage from './components/avnHomePage.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AvnHomePage
   }
 }
 </script>
 
 <style>
+html{
+  overflow-x: hidden !important;
+}
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/avnHomePage.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="avn-home-page">
+      <div class="avn-home-page__header">
+          <div class="avn-home-page__header-left">
+               <img class="" src="" alt="LOGO">
+          </div>
+          <div class="avn-home-page__header-right">
+              <ul class="avn-home-page__header-right-labels">
+                  <li v-for="(label, key) in navigationLabels" :key="key">
+                      <a href="javascript:void(0)"  class="avn-home-page__header-label">{{label}}</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+      <div class="avn-home-page__image">
+          <div class="avn-home-page__image-text">
+               <p class="avn-home-page__image-text-header">AVN Green Energy</p>
+               <p class="avn-home-page__image-text-sub-header">This plant</p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvnHomePAge',
+  components:{
+  },
+  data () {
+      return {
+          navigationLabels: ['Home', 'About', 'Contact']
+      }
+  }
+}
+</script>
+<style lang="scss">
+    .avn-home-page {
+        .avn-home-page__header {
+            display: flex;
+            align-items: center;
+            padding: 8px 40px;
+            // > * {
+            //     flex: 50%;
+            // }
+            .avn-home-page__header-left{
+                flex: 1;
+            }
+        }
+        .avn-home-page__header-right-labels {
+            display: flex;
+            list-style: none;
+            > * {
+                margin-right: 12px;
+            }
+        }
+        .avn-home-page__header-label {
+            color: black;
+            text-decoration: none;
+        }
+        .avn-home-page__image {
+            height: 670px;
+            object-fit: cover;
+            display: block;
+            background-size: cover;
+            background-image: url("./../assets/FloatingDrum.png");
+            background-repeat: no-repeat;
+            background-position: 50% 50%;
+            position: relative;
+            .avn-home-page__image-text {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                z-index: 2;
+                color: white;
+                font-family: patua-one;
+                white-space: nowrap;
+                > * {
+                    padding: 8px 0;
+                    margin: 0;
+                }
+                .avn-home-page__image-text-header {
+                     font-size: 5.625rem;
+                }
+                .avn-home-page__image-text-sub-header {
+                     font-size: 2rem;
+                }
+            }
+        }
+    }
+    @media only screen and (max-width: 600px) {
+        .avn-home-page__image{
+            .avn-home-page__image-text{
+                .avn-home-page__image-text-header {
+                    font-size: 2.65rem !important;
+                }
+                .avn-home-page__image-text-sub-header {
+                     font-size: 1.65rem !important;
+                }
+            }
+        }
+    }
+</style>
